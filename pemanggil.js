12514b53281529ce6ff464f540890443413f6541
ainul01 <35048148+ainul01@users.noreply.github.com>
2018-01-03 10:57:00 +0700

    Add files via upload

--- pemanggil.js
+++ pemanggil.js
@@ -25,7 +25,7 @@ if(x=="1"){xxx="/2018/01/go_82.html"}
 if(x=="2"){xxx="/2018/01/direct.html"}
 if(x=="3"){xxx="/2018/01/get.html"}
 if(x=="4"){xxx="/2018/01/redirect.html"}
-if(x=="5"){xxx="/"}a_to_ve[i].href="https://be-url.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
+if(x=="5"){xxx="/2018/01/generate.html"}a_to_ve[i].href="https://be-url.blogspot.com"+xxx+"?url="+ encryptedUrl;a_to_ve[i].rel="nofollow";a_to_vb++;a_to_vc+=i+":::"+ a_to_ve[i].href+"\n";}}
 var a_to_vj=document.getElementById("anonyminized");var a_to_vk=document.getElementById("found_links");if(a_to_vj)
 {a_to_vj.innerHTML+=a_to_vb;}
 if(a_to_vk)
